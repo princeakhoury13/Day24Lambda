486b001830973c7d92ee27011133de37714ac26d
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-11 18:26:06 +0530

    added: Master branch greeting message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day24Lambda
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the master branch of Assignment: Day 24");
+        }
+    }
+}
