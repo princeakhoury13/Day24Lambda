702959fb57efa9a671080c2c5358b928d66e6fbf
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-11 18:32:12 +0530

    added: Using Lambda Function to validate email

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Day24Lambda
+﻿using System.Text.RegularExpressions;
+
+namespace Day24Lambda
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the master branch of Assignment: Day 24");
+            Console.WriteLine("Enter your Email Id: ");
+            string name = Console.ReadLine();
+
+            Func<string, bool> validate = (email) =>
+                Regex.IsMatch(email, "^[A-Za-z0-9]+(.[a-zA-Z0-9]+)?@[a-zA-Z]+.[a-zA-Z]{2,}(.[A-Za-z]{2,})?$");
+
+            if (validate(name))
+            {
+                Console.WriteLine("Valid Email Id");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Email Id!");
+            }
         }
     }
 }
